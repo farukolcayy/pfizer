6a23f65e59bc22ec62fa20c7b3ce51f35b19a4d9
ömer faruk Olcay <53448620+farukolcayy@users.noreply.github.com>
2023-03-17 17:11:38 +0300

    public update

--- getStreamInfo.php
+++ getStreamInfo.php
@@ -6,7 +6,7 @@ $data = array();
 
 try {
 
-    $conn = new PDO('mysql:host=5.2.84.96;dbname=badiwork_pfizer;charset=utf8;port=3306', 'badiwork_pfizer', 'Ok?2021?.');
+    $conn = new PDO(-);
 
     $query1 = $conn->query("SELECT * FROM canli_yayin WHERE Id=1", PDO::FETCH_ASSOC);
     $result1 = $query1->fetchAll();
